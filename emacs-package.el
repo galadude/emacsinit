308e98398198e72030147c3538e235b308fd587e
Michael Carter Bisgaard Galanakis <vhg505@sund.root.ku.dk>
2016-05-06 13:29:48 +0200

    added script for install packages

---
+++ emacs-package.el
@@ -0,0 +1,23 @@
+
+;; my packages
+(setq my-packages
+      '(
+	evil
+	evil-magit
+	ess
+	auctex
+	helm
+	auto-complete
+	ac-helm
+	evil-leader
+	evil-surround
+	key-chord
+))
+
+;; refresh package list if it is not already available
+(when (not package-archive-contents) (package-refresh-contents))
+
+;; install packages from the list that are not yet installed
+(dolist (pkg my-packages)
+    (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
+        (package-install pkg)))
